0723e72c452c7c2d2df3e7e26dfe288cff4f5a31
Robin Yan <kitzoyan2@gmail.com>
2026-09-24 01:20:21 -0400

    8 bit counter with a few tests
    Async reset, sync load
    8-bit bus to queue load values
    output enable to close/open (hi Z) output lines

--- src/project.v
+++ src/project.v
@@ -5,23 +5,46 @@
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
+module tt_um_eight_bit_counter ( // define all your ports
+  input wire [7:0] ui_in,    // Load inputs
+  output wire [7:0] uo_out,   // Tri state outputs
+  
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  input wire ena,      // always 1 when the design is powered, so you can ignore it
+  input wire clk,      // clock
+  input wire rst_n     // reset_n - low to reset
+);
+  reg [7:0] count; // internal variable/state
+  wire ld = uio_in[0];
+  wire en_out = uio_in[1]; // for making output high impedance
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uio_out = 8'h00;
+  assign uio_oe  = 8'h00;
+
+  // always @(*) ... PROCEDURAL COMBINATIONAL, equivalent to continuous (assign) as 
+  // behavior is determined when any dependent signal is changed. Better for larger if/else ladders
+
+  always @(posedge clk or negedge rst_n) begin // SEQUENTIAL, behavior that is defined on a clock signal
+    
+    if (!rst_n) begin
+      count <= 8'h00;
+    end else if (ld) begin
+      count <= ui_in;
+    end else begin
+      count <= count + 1; // will naturally wrap from 255 to 0
+    end
+  end
+  
+
+  assign uo_out = en_out ? count : 8'hzz; // if not enabled, high impedance
+
+  
+  //wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
+
--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-
+from cocotb.types import Logic
 
 @cocotb.test()
 async def test_project(dut):
@@ -25,16 +25,48 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Test counter ability
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0b10 # enable output, no load
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    for i in range(256):
+        # Wait for one clock cycle to see the output values
+        await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+        # The following assersion is just an example of how to check the output values.
+        # Change it to match the actual expected output of your module:
+        assert dut.uo_out.value == i
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+    dut._log.info("Counter passed all 256 units")
+    # check for high impedance, then check load, and then reset
+    dut._log.info("Testing High impedance state")
+    dut.uio_in.value = 0b00
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 1)
+    assert str(dut.uo_out.value) == 'ZZZZZZZZ', "Output was not high impedance"
+    dut.uio_in.value = 0b10
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b11
+    assert dut.uo_out.value == 1, "Value did not wrap around"
+    await ClockCycles(dut.clk, 2) # Counter needs 2 cycles to take on load values
+    dut._log.info("Testing load enable")
+    assert dut.uo_out.value == 8, (f"Output was not set by load value: {dut.uo_out.value}") 
+    dut.uio_in.value = 0b10
+    await ClockCycles(dut.clk, 2) # Counter needs 2 cycles to take on load values
+    dut._log.info("Testing counter after load enable")
+    for i in range(9, 12):
+        assert dut.uo_out.value == i, "Counter did not resume after loading"
+        await ClockCycles(dut.clk, 1)
+    dut._log.info("Testing reset hold")
+    dut.rst_n.value = 0 
+    await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == 0
+    dut.rst_n.value = 1 
+    await ClockCycles(dut.clk, 1)
+    dut._log.info("Testing load hold")
+    dut.uio_in.value = 0b11
+    await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == 8
+    
